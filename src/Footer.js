85ac891a0717cdd63226e8051bda884c354c7f97
Gokulcasper <ronitcasper215@gmail.com>
2022-10-09 21:44:20 +0530

    Feat : Fixed Error

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-// import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
+
 
 function Footer() {
   return (
@@ -8,10 +8,7 @@ function Footer() {
       className="footer d__flex align__items__center pz-10"
       style={{ padding: "10px 20px", zIndex: "100" }}
     >
-      {/* <img src={footerImg} alt="" className="footer__img pointer" /> */}
-      {/* <div className="contact__socialIcons">
-        <a href="https://github.com/Gokulcasper"><FaGithub /></a>
-      </div> */}
+
       <span
         className="copyright"
         style={{ color: "#c6c9d8", fontSize: "14px", opacity: "0.75", marginLeft: "5px" }}
